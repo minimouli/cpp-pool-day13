3a868aa170ff7bcd15bef83d464ccce958f7a999
Adrien Belminksy <belminksy@gmail.com>
2023-01-29 07:08:54 +0100

    feat(suite): add ex05 suite

---
+++ res/ex05/main.cpp
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <iostream>
+#include "SharedPointer.hpp"
+#include "../Logger.hpp"
+
+int main() {
+
+    std::cout << "create ptr1 without value" << std::endl;
+    SharedPointer<Logger> ptr1;
+
+    std::cout << "ptr1.get() = " << (ptr1.get() == nullptr ? "nullptr" : "not null") << std::endl;
+
+    std::cout << "set ptr1 with Logger(1)" << std::endl;
+    ptr1 = new Logger(1);
+
+    std::cout << "ptr1.get() = " << (ptr1.get() == nullptr ? "nullptr" : "not null") << std::endl;
+    std::cout << "ptr1->sayHello()" << std::endl;
+    ptr1->sayHello();
+
+    std::cout << "---" << std::endl;
+
+    std::cout << "create ptr2 with new Logger(2)" << std::endl;
+    SharedPointer ptr2(new Logger(2));
+
+    std::cout << "ptr2->sayHello()" << std::endl;
+    ptr2->sayHello();
+    std::cout << "ptr2.get()->sayHello()" << std::endl;
+    ptr2.get()->sayHello();
+
+    std::cout << "ptr2.get() = " << (ptr2.get() == nullptr ? "nullptr" : "not null") << std::endl;
+    std::cout << "ptr2.reset()" << std::endl;
+    ptr2.reset();
+    std::cout << "ptr2.get() = " << (ptr2.get() == nullptr ? "nullptr" : "not null") << std::endl;
+
+    std::cout << "---" << std::endl;
+
+    std::cout << "create ptr3 with new Logger(3)" << std::endl;
+    SharedPointer ptr3(new Logger(3));
+
+    std::cout << "ptr3->sayHello()" << std::endl;
+    ptr3->sayHello();
+
+    std::cout << "set ptr3 with new Logger(4)" << std::endl;
+    ptr3 = new Logger(4);
+
+    std::cout << "ptr3->sayHello()" << std::endl;
+    ptr3->sayHello();
+
+    std::cout << "---" << std::endl;
+
+    std::cout << "create ptr4 with new Logger(5)" << std::endl;
+    SharedPointer ptr4(new Logger(5));
+    std::cout << "create ptr5 with ptr4" << std::endl;
+    SharedPointer ptr5 = ptr4;
+    std::cout << "create ptr6 without value" << std::endl;
+    SharedPointer<Logger> ptr6;
+
+    std::cout << "ptr4->sayHello()" << std::endl;
+    ptr4->sayHello();
+    std::cout << "ptr5->sayHello()" << std::endl;
+    ptr5->sayHello();
+    std::cout << "ptr6.get() = " << (ptr6.get() == nullptr ? "nullptr" : "not null") << std::endl;
+
+    std::cout << "set ptr6 with ptr5" << std::endl;
+    ptr6 = ptr5;
+
+    std::cout << "ptr4->sayHello()" << std::endl;
+    ptr4->sayHello();
+    std::cout << "ptr5->sayHello()" << std::endl;
+    ptr4->sayHello();
+    std::cout << "ptr6->sayHello()" << std::endl;
+    ptr4->sayHello();
+
+    std::cout << "ptr5.reset()" << std::endl;
+    ptr5.reset();
+
+    std::cout << "ptr4->sayHello()" << std::endl;
+    ptr4->sayHello();
+    std::cout << "ptr6->sayHello()" << std::endl;
+    ptr6->sayHello();
+
+    std::cout << "set ptr4 with new Logger(6)" << std::endl;
+    ptr4 = new Logger(6);
+
+    std::cout << "ptr4->sayHello()" << std::endl;
+    ptr4->sayHello();
+    std::cout << "ptr6->sayHello()" << std::endl;
+    ptr6->sayHello();
+
+    std::cout << "set ptr6 with nullptr" << std::endl;
+    ptr6 = nullptr;
+
+    std::cout << "ptr4->sayHello()" << std::endl;
+    ptr4->sayHello();
+    std::cout << "ptr6.get() = " << (ptr6.get() == nullptr ? "nullptr" : "not null") << std::endl;
+
+    std::cout << "---" << std::endl;
+
+    return 0;
+}
---
+++ src/ex05.suite.ts
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { GPP, Path, expect, suite, test } from '@minimouli/framework'
+
+suite('Exercise 05', () => {
+
+    test('Shared Pointer', async () => {
+
+        const gpp = new GPP([
+            Path.fromMoulinette('./res/ex05/main.cpp'),
+            Path.fromMoulinette('./res/Logger.cpp')
+        ])
+        const exec = await gpp.execute([
+            `-I${Path.fromProject().toString()}`,
+            '-std=c++20'
+        ])
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).toOutput([
+            'create ptr1 without value',
+            'ptr1.get() = nullptr',
+            'set ptr1 with Logger(1)',
+            '#1 -> construction',
+            'ptr1.get() = not null',
+            'ptr1->sayHello()',
+            '#1 -> wesh alors',
+            '---',
+            'create ptr2 with new Logger(2)',
+            '#2 -> construction',
+            'ptr2->sayHello()',
+            '#2 -> wesh alors',
+            'ptr2.get()->sayHello()',
+            '#2 -> wesh alors',
+            'ptr2.get() = not null',
+            'ptr2.reset()',
+            '#2 -> destruction',
+            'ptr2.get() = not null',
+            '---',
+            'create ptr3 with new Logger(3)',
+            '#3 -> construction',
+            'ptr3->sayHello()',
+            '#3 -> wesh alors',
+            'set ptr3 with new Logger(4)',
+            '#4 -> construction',
+            '#3 -> destruction',
+            'ptr3->sayHello()',
+            '#4 -> wesh alors',
+            '---',
+            'create ptr4 with new Logger(5)',
+            '#5 -> construction',
+            'create ptr5 with ptr4',
+            'create ptr6 without value',
+            'ptr4->sayHello()',
+            '#5 -> wesh alors',
+            'ptr5->sayHello()',
+            '#5 -> wesh alors',
+            'ptr6.get() = nullptr',
+            'set ptr6 with ptr5',
+            'ptr4->sayHello()',
+            '#5 -> wesh alors',
+            'ptr5->sayHello()',
+            '#5 -> wesh alors',
+            'ptr6->sayHello()',
+            '#5 -> wesh alors',
+            'ptr5.reset()',
+            'ptr4->sayHello()',
+            '#5 -> wesh alors',
+            'ptr6->sayHello()',
+            '#5 -> wesh alors',
+            'set ptr4 with new Logger(6)',
+            '#6 -> construction',
+            'ptr4->sayHello()',
+            '#6 -> wesh alors',
+            'ptr6->sayHello()',
+            '#5 -> wesh alors',
+            'set ptr6 with nullptr',
+            '#5 -> destruction',
+            'ptr4->sayHello()',
+            '#6 -> wesh alors',
+            'ptr6.get() = nullptr',
+            '---',
+            '#6 -> destruction',
+            '#4 -> destruction',
+            '#1 -> destruction',
+            ''
+        ])
+    })
+
+})
